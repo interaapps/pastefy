f814ebaf64834f783400fff008db73c81fe4a7b3
JulianFun123 <julianfun123@yahoo.com>
2019-06-23 09:01:10 +0200

    dev

--- app/route.php
+++ app/route.php
@@ -16,6 +16,5 @@ $route = [
   "/"                        =>     "homepage.php",
   "/about"                   =>     "!AboutController@about", // Executing the static about function in the AboutController class
   "/d/([a-z]*)"              =>     "customtest.php",
-  "@__404__@"                =>     "404.php",
-  "/example"                  =>     "example.php"
+  "@__404__@"                =>     "404.php"
 ];
