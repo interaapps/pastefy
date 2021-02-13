3fd0e9f2b4568bbd840a65225fe51683122d3004
Julian Gojani <julian@gojani.xyz>
2021-02-13 12:23:35 +0100

    Added content-type for raw content

--- backend/src/main/java/de/interaapps/pastefy/controller/RawController.java
+++ backend/src/main/java/de/interaapps/pastefy/controller/RawController.java
@@ -3,6 +3,7 @@ package de.interaapps.pastefy.controller;
 import de.interaapps.pastefy.model.database.Paste;
 import org.javawebstack.framework.HttpController;
 import org.javawebstack.httpserver.Exchange;
+import org.javawebstack.httpserver.helper.MimeType;
 import org.javawebstack.httpserver.router.annotation.Get;
 import org.javawebstack.httpserver.router.annotation.Path;
 import org.javawebstack.orm.Repo;
@@ -12,6 +13,7 @@ public class RawController extends HttpController {
     @Get("/{id}/raw")
     public String getPasteRaw(Exchange exchange, @Path("id") String id){
         Paste paste = Repo.get(Paste.class).where("key", id).first();
+        exchange.contentType(MimeType.PLAIN);
         if (paste == null) {
             exchange.status(404);
             return "404 - Paste not found";
