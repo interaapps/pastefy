40c850b7a91336275cb8baa0a8d4de4ec463f5dc
Julian Gojani <julian@gojani.xyz>
2021-09-20 22:44:42 +0200

    Removed IaAuth and changed to InteraApps OAuth2 (Google, Github, Discord and Twitch is now usable as well)

---
+++ frontend/src/views/auth/LoginSelection.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Log in with</h1>
+        <a v-for="authtype of $store.state.user.auth_types" :key="authtype" :href="'/api/v2/auth/oauth2/'+authtype" style="display: block; margin-bottom: 20px;">Login with your {{authtype}}-Account</a>
+    </div>
+</template>
+<script>
+export default {
+    mounted(){
+    }
+}
+</script>
