64617f7e6c0944fed506dd185075ad01d4f9a6f1
Julian Gojani <julian@gojani.xyz>
2021-10-10 16:43:48 +0200

    Added more languages

--- backend/src/test/java/asdf.java
+++
@@ -1,8 +0,0 @@
-import org.javawebstack.httpclient.HTTPClient;
-
-public class asdf {
-    public static void main(String[] args) {
-        HTTPClient httpClient = new HTTPClient();
-        System.out.println(httpClient.get("http://js.gjni.eu").string());
-    }
-}
